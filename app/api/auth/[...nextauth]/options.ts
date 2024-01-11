defeabcc9400c1a376426be8ed636704ef68dfce
Dejan Davidović <dejandavidovict@gmail.com>
2024-01-11 13:58:43 +0100

    nextauthjs credentials

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,49 @@
+import type { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const options: NextAuthOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username:",
+          type: "text",
+          placeholder: "Username",
+        },
+        password: {
+          label: "Password:",
+          type: "password",
+          placeholder: "Password",
+        },
+      },
+      async authorize(credentials) {
+        const res = await fetch("http://localhost:29468/api/login", {
+          method: "POST",
+          body: JSON.stringify(credentials),
+          headers: { "Content-Type": "application/json" },
+        });
+        const user = await res.json();
+
+        // If no error and we have user data, return it
+        if (res.ok && user) {
+          return user;
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user };
+    },
+    async session({ session, token }) {
+      session.user = token as any;
+      return session;
+    },
+  },
+};
---
+++ app/projects/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <>
+      <h2>Categories</h2>
+      <hr />
+    </>
+  );
+};
+
+export default Projects;
